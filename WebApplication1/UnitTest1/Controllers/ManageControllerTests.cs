40803b2d6844799990bf9ed4c40cac409c50457c
Glen-Lucero <glucero44@gmail.com>
2016-12-05 19:35:03 -0500

    Merge

---
+++ WebApplication1/UnitTest1/Controllers/ManageControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers.Tests
+{
+    [TestClass()]
+    public class ManageControllerTests
+    {
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            ManageController testcontroller = new ManageController();
+            DeleteViewModel model = new DeleteViewModel()
+            {
+
+            };
+
+            var result = testcontroller.Delete(model) as Task<ActionResult>;
+
+            Assert.AreEqual(403, result);
+
+        }
+    }
+}
